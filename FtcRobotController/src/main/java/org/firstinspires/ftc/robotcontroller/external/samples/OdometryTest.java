230ef8d28e5efdd0150eda92897002a50f125fd1
Domenic Belmonte <djbelmonte1@gmail.com>
2020-07-27 21:01:49 -0500

    Odometry program w/ 4 motors

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/OdometryTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/OdometryTest.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "OdometryTest")
 public class OdometryTest extends LinearOpMode {
 
     private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
 
     private int range = 10;
 
@@ -18,14 +22,16 @@ public class OdometryTest extends LinearOpMode {
 
         setUp();
 
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor4.setDirection(DcMotorSimple.Direction.REVERSE);
+
         motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        //motor1.setTargetPosition(1120);
-
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         while(opModeIsActive()) {
-// This is a test to see if I can pull without permission
-            if(runTo(motor1, 1000)) {
+            if(runTo(motor1, motor2, motor3, motor4, 1000)) {
                 break;
             }
 
@@ -36,25 +42,39 @@ public class OdometryTest extends LinearOpMode {
     }
     private void setUp(){
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motor4 = hardwareMap.get(DcMotor.class, "motor4");
     }
 
-    private boolean runTo(DcMotor motorOne, int Target){
+    private boolean runTo(DcMotor motorOne, DcMotor motorTwo, DcMotor motorThree, DcMotor motorFour, int Target){
 
-        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorOne.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorTwo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorThree.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFour.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         int Pos = motorOne.getCurrentPosition();
-
 
         while(Target != Pos && opModeIsActive()){
             Pos = motorOne.getCurrentPosition();
             if(Pos < Target + range){
                 motorOne.setPower(-1);
+                motorTwo.setPower(-1);
+                motorThree.setPower(-1);
+                motorFour.setPower(-1);
             }
             else if(Pos > Target - range){
                 motorOne.setPower(1);
+                motorTwo.setPower(1);
+                motorThree.setPower(1);
+                motorFour.setPower(1);
             }
             else if(Pos <= Target + range && Pos >= Target - range){
                 motorOne.setPower(0);
+                motorTwo.setPower(0);
+                motorThree.setPower(0);
+                motorFour.setPower(0);
                 return true;
             }
         }
