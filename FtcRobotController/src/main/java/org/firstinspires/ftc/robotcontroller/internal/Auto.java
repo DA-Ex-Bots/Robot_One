8c70730631909264aa4d87838257e182ddeec5c4
bsprouts10 <54560436+bsprouts10@users.noreply.github.com>
2021-01-11 20:05:11 -0600

    Update Auto.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Auto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Auto.java
@@ -26,131 +26,75 @@ public class Auto extends LinearOpMode {
 
         waitForStart();
 
+        robot.servo2.setPosition(0.3);
+
         // Moves forward from wall
-        robot.Straight(36);
+        robot.Straight(-36);
         sleep(500);
 
         // Strafes left
-        robot.Left_Strafe(2);
+        robot.Left_Strafe(-2);
         sleep(500);
 
         // ZONE B
 
         if(pos == 1){
-            // Moves forward to zone B
-            robot.Straight(30);
+            robot.Straight(-30);
             sleep(500);
 
             // Strafes left
-            robot.Left_Strafe(20);
+            robot.Left_Strafe(-20);
             sleep(500);
 
             // Moves forward into zone B
-            robot.Straight(15);
+            robot.Straight(-15);
             sleep(500);
 
-
-            // Moves backward from zone B
-            robot.Straight(-12);
-            sleep(500);
-
-            // Strafes right
-            robot.Right_Strafe(22);
-            sleep(500);
-
-            // Moves forward to low goal
-            robot.Straight(48);
-            sleep(500);
-
-            //Strafes right
-            robot.Right_Strafe(28);
-            sleep(500);
-
-            // Moves forward to low goal
-            robot.Straight(16);
-            sleep(500);
-
-            // Drops ring into low goal
-            robot.motor5.setPower(-1);
-            sleep(3000);
-            robot.motor5.setPower(0);
-
-            // Moves backward from low goal
-            robot.Straight(-10);
-            sleep(500);
-
-            // Strafes right
-            robot.Right_Strafe(10);
-            sleep(500);
+            // Drops wobble goal into zone B
+            robot.servo2.setPosition(0);
+            sleep(1000);
 
             // Moves backward onto line
-            robot.Straight(-50);
+            robot.Straight(24);
         }
 
         // ZONE C
 
         else if(pos == 2){
             // Moves forward into zone C
-            robot.Straight(76);
+            robot.Straight(-76);
             sleep(500);
 
             // Strafes right
-            robot.Right_Strafe(12);
+            robot.Right_Strafe(-12);
             sleep(500);
 
-
-            // Moves backward from zone C
-            robot.Straight(-12);
-            sleep(500);
-
-            // Strafes left
-            robot.Left_Strafe(24);
-            sleep(500);
-
-            // Moves forward to low goal
-            robot.Straight(40);
-            sleep(500);
-
-            // Drops ring into low goal
-            robot.motor5.setPower(-1);
-            sleep(3000);
-            robot.motor5.setPower(0);
+            // Drops wobble goal into zone C
+            robot.servo2.setPosition(0);
+            sleep(1000);
 
             // Moves backward onto line
-            robot.Straight(-50);
+            robot.Straight(50);
         }
 
         // ZONE A
 
         else {
             // Moves forward into zone A
-            robot.Straight(20);
+            robot.Straight(-20);
             sleep(500);
 
             // Strafes right
-            robot.Right_Strafe(12);
+            robot.Right_Strafe(-12);
             sleep(500);
 
-
-            // Moves backward from zone A
-            robot.Straight(-12);
-            sleep(500);
-
-            // Strafes left
-            robot.Left_Strafe(24);
-            sleep(500);
-
-            // Moves forward to low goal
-            robot.Straight(92);
-            sleep(500);
-
-            // Drops ring into low goal
-            robot.motor5.setPower(-1);
-            sleep(4000);
-            robot.motor5.setPower(0);
+            // Drops wobble goal into zone A
+            robot.servo2.setPosition(0);
+            sleep(1000);
 
             // Moves backward onto line
-            robot.Straight(-50);
+            robot.Straight(5);
         }
+        robot.Left_Strafe(10);
     }
 }
